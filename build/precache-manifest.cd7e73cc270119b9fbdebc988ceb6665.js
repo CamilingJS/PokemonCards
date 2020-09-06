fdbc9001ddfaf35a53b2fb37de8c7b7cff5cf58a
Jonathan <jonathancamiling@gmail.com>
2020-09-06 16:18:50 -0700

    second init

---
+++ build/precache-manifest.cd7e73cc270119b9fbdebc988ceb6665.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b1b0b941c0053dcc541b0e2f32068d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d73f5c2031098bdde8c",
+    "url": "/static/css/main.b0e9ea26.chunk.css"
+  },
+  {
+    "revision": "a392ec5b8c70258f4e4c",
+    "url": "/static/js/2.aa9473f7.chunk.js"
+  },
+  {
+    "revision": "3d73f5c2031098bdde8c",
+    "url": "/static/js/main.6dd4bebe.chunk.js"
+  },
+  {
+    "revision": "ee427654517dbeb0985f",
+    "url": "/static/js/runtime-main.008b31b7.js"
+  }
+]);
